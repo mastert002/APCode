b97a4c2033409e53fe727d352f3c877f02a37849
mastert002 <46490384+mastert002@users.noreply.github.com>
2023-05-23 07:50:00 +0100

    Initial commit

---
+++ appBar Hide/src/extensions/appBarHide/AppBarHideApplicationCustomizer.ts
@@ -0,0 +1,34 @@
+import { override } from "@microsoft/decorators";
+import { Log } from "@microsoft/sp-core-library";
+import { BaseApplicationCustomizer } from "@microsoft/sp-application-base";
+import { Dialog } from "@microsoft/sp-dialog";
+
+import * as strings from "AppBarHideApplicationCustomizerStrings";
+
+const LOG_SOURCE: string = "AppBarHideApplicationCustomizer";
+
+/**
+ * If your command set uses the ClientSideComponentProperties JSON input,
+ * it will be deserialized into the BaseExtension.properties object.
+ * You can define an interface to describe it.
+ */
+export interface IAppBarHideApplicationCustomizerProperties {
+  // This is an example; replace with your own property
+  elementId: string;
+}
+
+/** A Custom Action which can be run during execution of a Client Side Application */
+export default class AppBarHideApplicationCustomizer extends BaseApplicationCustomizer<IAppBarHideApplicationCustomizerProperties> {
+  @override
+  public onInit(): Promise<void> {
+    if (this.properties.elementId != "") {
+      const spElement: any = document.getElementById(this.properties.elementId);
+
+      Log.info(LOG_SOURCE, `Found Element ${spElement}`);
+
+      spElement.style = "display:none";
+    }
+
+    return Promise.resolve();
+  }
+}
